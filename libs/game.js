25e33acf8ea574971128e86938ab1ef7902f2b68
ChocolateCode4 <AZULINROSE@GMAIL.COM>
2020-03-02 18:56:28 +0800

    WebGL ide testing

--- libs/game.js
+++ libs/game.js
@@ -1,4 +1,32 @@
 //import * as THREE from 'three.module.js';
 
 const scene = new THREE.Scene();
-console.log("Template Working!");
+const camera = new THREE.PerspectiveCamera(
+  75, 
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+//sandbox testing for rendering:
+const geomBox = new THREE.BoxGeometry(2,2,2,2);
+const boxMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00});
+const box = THREE.Mesh(geomBox, boxMaterial);
+scene.add(box);
+
+camera.position.z = 10;
+camera.position.x = 0;
+camera.position.y = 0;
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+
+animate();
